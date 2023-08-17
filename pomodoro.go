5c25610050387030b6d5270f824bfa4b08b90477
Rolly Maulana Awangga <awangga@gmail.com>
2023-08-17 21:03:44 +0700

    sip

--- pomodoro.go
+++ pomodoro.go
@@ -60,3 +60,38 @@ func main() {
 	simpleCountdownBreak(finish, formatter, X, Y)
 
 }
+
+func GetSetTime(status string) (finish time.Time, formatter func(time.Duration) string, err error) {
+	nokiaTune()
+	start := time.Now()
+	if status == "task" {
+		finish, err = waitDuration(start)
+	} else {
+		finish, err = waitBreakDuration(start)
+	}
+
+	if err != nil {
+		flag.Usage()
+		os.Exit(2)
+	}
+	wait := finish.Sub(start)
+	fmt.Printf("Start timer for %s.\n\n", wait)
+	formatter = formatSeconds
+	switch {
+	case wait >= 24*time.Hour:
+		formatter = formatDays
+	case wait >= time.Hour:
+		formatter = formatHours
+	case wait >= time.Minute:
+		formatter = formatMinutes
+	}
+	beeep.Notify("Pomodoro Info", "Start Melakukan Task 25 menit", "assets/information.png")
+	if status == "task" {
+		simpleCountdown(finish, formatter)
+		TakeScreenshot()
+	} else {
+		X, Y := robotgo.GetMousePos()
+		simpleCountdownBreak(finish, formatter, X, Y)
+	}
+	return
+}
