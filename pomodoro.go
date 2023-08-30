ab5f441ebfa510b185c3a2a99cd9edcdeb5db49c
Rolly Maulana Awangga <awangga@gmail.com>
2023-08-30 19:59:33 +0700

    remove assets folder

--- pomodoro.go
+++ pomodoro.go
@@ -42,7 +42,7 @@ func main() {
 	SendReportTo(filename, wag)
 	msg := "Selamat!!!!! 1 sesi pomodoro selesai dengan jumlah skrinsutan:" + strconv.Itoa(len(ScreenShotStack))
 
-	beeep.Alert("Pomodoro Info", msg, "assets/information.png")
+	beeep.Alert("Pomodoro Info", msg, "information.png")
 	fmt.Println("1 sesi pomodoro selesai dengan jumlah skrinsutan:", strconv.Itoa(len(ScreenShotStack)))
 	// Listen to Ctrl+C (you can also do something else that prevents the program from exiting)
 	c := make(chan os.Signal)
