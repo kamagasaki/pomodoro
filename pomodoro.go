40e67a74e1ad7a8f6a1b50020b1a338bc6f8d8be
Rolly Maulana Awangga <awangga@gmail.com>
2023-08-30 19:07:47 +0700

    release

--- pomodoro.go
+++ pomodoro.go
@@ -5,7 +5,10 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
+	"strconv"
 	"syscall"
+
+	"github.com/gen2brain/beeep"
 )
 
 func main() {
@@ -25,11 +28,14 @@ func main() {
 	GetSetTime("break")
 
 	GetSetTime("longbreak")
+
 	img := GetRandomScreensot(ScreenShotStack)
 	filename := ImageToFile(img)
 	SendReportTo(filename, "6281313112053-1492882006")
+	msg := "Selamat!!!!! 1 sesi pomodoro selesai dengan jumlah skrinsutan:" + strconv.Itoa(len(ScreenShotStack))
 
-	fmt.Println("1 sesi pomodoro selesai")
+	beeep.Alert("Pomodoro Info", msg, "assets/information.png")
+	fmt.Println("1 sesi pomodoro selesai dengan jumlah skrinsutan:", strconv.Itoa(len(ScreenShotStack)))
 	// Listen to Ctrl+C (you can also do something else that prevents the program from exiting)
 	c := make(chan os.Signal)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
